f2f29a38c4cf04932e70087a698f7e03e2aa5a09
João Beraldo <joaovic0503@gmail.com>
2025-05-13 17:23:28 -0300

    Correção na seleção de botões

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from main import DataStore, FlashcardApp
 import random
 
 DATA_FILE = "data.json"
-st.title("Flashcard App")
+st.title("O que vamos traduzir hoje?")
 
 def init_session():
     st.session_state.setdefault("cards", [])
@@ -13,30 +13,62 @@ def init_session():
         datastore.create_file()
     else:
         st.session_state.cards = state["cards"]
+        st.session_state.progress = state["progress"]
         
 def show_create_card():
-    user_input = st.text_input("Texto original")
-    if st.button("Traduzir"):
-        card = FlashcardApp.create_card(user_input)  # retorna card
-        # Exibir o resultado na tela
-        st.write(card["output"])
-        st.session_state.cards.append(card)
+    with st.container():
+        user_input = st.chat_input("Escreva o texto a ser traduzido")
+        if user_input:
+            app = FlashcardApp()
+            card = app.create_card(user_input)  # retorna card
+            st.write(card["output"])
+            st.success("Card criado e pronto para revisão!")
+            st.session_state.cards.append(card)
+        
+
+def next_card(previous_card: dict): 
+    """
+    Input: Acão do usuário
+    Logic: 
+    1. Se o usuário clicar no botão
+    2. Exibir próximo card aleatório
+    Output: Exibir outro card, diferente do anterior
+    """
+    card = random.choice(st.session_state.cards)
+    if len(st.session_state.cards) == 1:
+        st.write("Não há mais cards para revisar.")
+    else: 
+        card["id"] != previous_card["id"]
+        return card
 
 
 def show_review_card():
     if not st.session_state.cards:
         st.write("Nenhum card criado ainda.")
         return
+    # escolhe e guarda o card atual
     card = random.choice(st.session_state.cards)
-    st.write("Texto:")
-    st.write(card["input"])
-    user_output = st.text_input("Escreva a resposta")
-    if st.button("Revisar"):            
-        st.write("Resposta correta:", card["output"])
-        app = FlashcardApp()
-        feedback = app.review_card(card=card, user_output=user_output)
-        st.write_stream(feedback)
+    st.session_state.current_card = card
 
+    progress = st.session_state.progress
+    st.write(card["input"]) # Não sendo atualizado ao clicar no botão de enviar, gerando inconsistência entre o output esperado em 
+    
+    # Exibir quantas vezes o card foi revisado
+    for p in progress:
+        if p["card_id"] == card["id"]:
+            st.write("Revisões:", p["reviews"])
+           
+    user_output = st.chat_input("Escreva a resposta")
+    if user_output:           
+        # st.write("Resposta correta:", card["output"])
+        app = FlashcardApp()
+        review = app.review_card(card=card, user_output=user_output)
+        st.write_stream(review)
+    # Botão para ir ao próximo card
+    if st.button("Próximo Card"):
+        # chama a função que seleciona outro card diferente
+        next_card(st.session_state.current_card)
+        
 
 def main():
     init_session()
